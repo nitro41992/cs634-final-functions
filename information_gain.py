d4e99ca7f85c298e8949f5c668ce4423fc992bbe
nitro41992 <nick.k@iton.net>
2019-12-12 20:16:32 -0500

    Task: Calculated Entropy

--- information_gain.py
+++ information_gain.py
@@ -1,30 +1,45 @@
 import sys
 import cmath
+import math
 import csv
 import pandas as pd
 
 
 data = pd.read_csv('data.txt', header=None)
-# print(data)
 features = data[0].unique()
 # print(features)
+
 
 for i in features:
     grouped = data.groupby(data.columns[0])
     sub = grouped.get_group(i)
 
+    p_tot = sub.sum()[1]
+    n_tot = sub.sum()[2]
+    full_tot = p_tot + n_tot
+    tot_I = -(p_tot / full_tot)*(cmath.log(p_tot/full_tot, 2)) - \
+        (n_tot / full_tot)*(cmath.log(n_tot/full_tot, 2))
+
+    out = []
     for index, row in sub.iterrows():
         p = float(row[1])
         n = float(row[2])
-        row_tot = p + n
+        tot = p + n
 
-        I = -(p / row_tot)*(cmath.log(p/row_tot, 2)) - \
-            (n / row_tot)*(cmath.log(n/row_tot, 2))
-        print(f'{list(row)},{round(I.real, 3)}')
+        I = -(p / tot)*(cmath.log(p/tot, 2)) - \
+            (n / tot)*(cmath.log(n/tot, 2))
 
-p = sub.sum()[1]
-n = sub.sum()[2]
-row_tot = p + n
-tot_I = -(p / row_tot)*(cmath.log(p/row_tot, 2)) - \
-    (n / row_tot)*(cmath.log(n/row_tot, 2))
-print(round(I.real, 3))
+        if cmath.isnan(I):
+            I = 0
+
+        out_row = (list(row), round(tot/full_tot, 3), round(I.real, 3))
+        out.append(out_row)
+
+    E = []
+    for x in range(len(out)):
+        E.append(out[x][1] * out[x][2])
+        Entropy = round(sum(E), 3)
+
+
+# print(
+#     f'Data: {list(row)} I(pi, ni): {round(I.real, 3)}  I(p,i): {round(tot_I.real, 3)}')
