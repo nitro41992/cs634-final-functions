ec197032146039fbc07c3d6df9931604d2028650
nitro41992 <nick.k@iton.net>
2019-12-13 02:50:28 -0500

    Task: Finished hac.py. Pending: Plots

--- test.py
+++ test.py
@@ -8,7 +8,12 @@ data = pd.read_csv('hac_data.txt')
 # print(data)
 
 df = []
+groups = []
 for i, row in data.iterrows():
+    coord = tuple(data.iloc[i].values)
+    vals = [coord, i]
+    groups.append(vals)
+
     for j, row in data.iterrows():
         x = data.iloc[i].values
         y = data.iloc[j].values
@@ -22,8 +27,40 @@ for i, row in data.iterrows():
             df.append(vals)
 
 
-dists = pd.DataFrame(df, columns=['x', 'y', 'Distance'])
+dists = pd.DataFrame(df, columns=['x', 'y', 'distance'])
 
-sorted_dists = dists.sort_values(by=['Distance'])
-dist_dists = sorted_dists.drop_duplicates('Distance')
-print(dist_dists)
+sorted_dists = dists.sort_values(by=['distance'])
+dist_dists = sorted_dists.drop_duplicates('distance').reset_index(drop=True)
+
+# print(dist_dists)
+# print(groups)
+
+df_groups = dists = pd.DataFrame(groups, columns=['point', 'group'])
+# print(dist_dists)
+# print(df_groups)
+
+
+# while df_groups.nunique(0)['group'] > 2:
+for l, d_row in dist_dists.iterrows():
+    a = dist_dists.iloc[l].values
+    x = df_groups.loc[df_groups['point'] == a[0], 'group'].values
+    y = df_groups.loc[df_groups['point'] == a[1], 'group'].values
+    # print(l)
+    print(
+        f'a[0] = {a[0]} pos of a[0] = {x[0]} a[1] = {a[1]}  pos of a[1] = {y[0]}')
+
+    if x[0] > y[0]:
+        df_groups.loc[df_groups['point'] == a[1], 'group'] = x[0]
+
+        df_groups['group'] = np.where(
+            df_groups['group'] == y[0], x[0], df_groups['group'])
+    else:
+        df_groups.loc[df_groups['point'] == a[1], 'group'] = y[0]
+        df_groups['group'] = np.where(
+            df_groups['group'] == x[0], y[0], df_groups['group'])
+
+    if df_groups.nunique(0)['group'] <= 2:
+        break
+
+
+print(df_groups)
